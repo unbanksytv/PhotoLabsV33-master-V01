98aa3b390a1f35931ee59c9c6023524a9302f7f5
UnbanksyTV <45092543+unbanksytv@users.noreply.github.com>
2022-07-25 18:55:29 +0200

    downgrade react-scripts to 4.0.3 to fix webpack 5

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import MyRouter from "routers/index";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
-
-function App() {
+const App = () => {
   return (
+    <ThirdwebProvider desiredChainId={ChainId.Mainnet}>
     <div className="bg-white text-base dark:bg-neutral-900 text-neutral-900 dark:text-neutral-200">
       <MyRouter />
     </div>
+    </ThirdwebProvider>
   );
 }
 
